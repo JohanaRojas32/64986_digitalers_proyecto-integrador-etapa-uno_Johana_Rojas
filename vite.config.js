c53493f32e9fa5db5f8c9db6cb948be1e0896985
Johana Rojas <joohanarojas96@gmail.com>
2023-08-22 01:12:15 -0600

    cree la carpeta dist con el npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+import { resolve } from 'node:path'
+
 export default {
     server: {
         port: "3232",
     },
     css : {
-        devSourcemap : true
+        devSourcemap : true,
+    },
+    build: {
+        emptyOurDir: true,
+        rollupOptions: {
+            input: {
+                carrito: resolve('pages/carrito.html'),
+                contacto: resolve('pages/contacto.html'),
+                nosotros: resolve('pages/nosotros.html'),
+                inicio: resolve('index.html')
+            }
+        }
     }
 }
